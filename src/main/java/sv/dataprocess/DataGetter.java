92ad674d2b5075aeeec46e4aff61c8ff1b570588
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-04-24 11:03:08 +0200

    no lfights category

--- src/main/java/sv/dataprocess/DataGetter.java
+++ src/main/java/sv/dataprocess/DataGetter.java
@@ -7,12 +7,11 @@ import com.github.dvdme.ForecastIOLib.ForecastIO;
 
 public class DataGetter {
 
-	private static Logger logger = LoggerFactory.getLogger(DataGetter.class);
-
 	public static void main(String[] args) {
 
 		try {
-			getTimeWheater("41.5", "0.5", 0, 0);
+			// longitu, latitud, offsetdia, offsethora
+			WeatherDate weather = getTimeWheater("41.5", "0.5", 0, 0);
 
 		} catch (Exception a) {
 		}
@@ -21,7 +20,7 @@ public class DataGetter {
 	/*
 	 * pasan la hora del lugar
 	 */
-	public static void getTimeWheater(String lon, String lat, int offsetdia, int offsethour) throws Exception {
+	public static WeatherDate getTimeWheater(String lon, String lat, int offsetdia, int offsethour) throws Exception {
 		// Gets the hour offset
 
 		// offSetDays,int hour,int minutes
@@ -33,7 +32,7 @@ public class DataGetter {
 		fio.getForecast(lon, lat);
 
 		if (offsetdia >= 1) {
-			offsethour+=offsetdia*24;
+			offsethour += offsetdia * 24;
 		}
 		String temp_c = "";//
 		String dewpoint_c = "";//
@@ -45,42 +44,50 @@ public class DataGetter {
 			// tiempo actual
 			FIOCurrently currently = new FIOCurrently(fio);
 			// Print currently data
-			dewpoint_c=String.valueOf(currently.get().dewPoint());
-			temp_c=String.valueOf(currently.get().temperature());
-			wind_speed_kt=String.valueOf(currently.get().windSpeed());
-			wind_dir_degrees=String.valueOf(currently.get().windBearing());
-			String cloud=String.valueOf(currently.get().cloudCover());
-			Double clou=Double.valueOf(cloud);
-			sky_cover="OVC";
-			if(clou<0.7)sky_cover="BKN";
-			if(clou<0.5)sky_cover="SCT";
-			if(clou<0.25)sky_cover="FEW";
-			if(clou<0.1)sky_cover="SKC";
+			dewpoint_c = String.valueOf(currently.get().dewPoint());
+			temp_c = String.valueOf(currently.get().temperature());
+			wind_speed_kt = String.valueOf(currently.get().windSpeed());
+			wind_dir_degrees = String.valueOf(currently.get().windBearing());
+			String cloud = String.valueOf(currently.get().cloudCover());
+			Double clou = Double.valueOf(cloud);
+			sky_cover = "OVC";
+			if (clou < 0.7)
+				sky_cover = "BKN";
+			if (clou < 0.5)
+				sky_cover = "SCT";
+			if (clou < 0.25)
+				sky_cover = "FEW";
+			if (clou < 0.1)
+				sky_cover = "SKC";
 
+			return new WeatherDate(temp_c, dewpoint_c, wind_dir_degrees, wind_speed_kt, sky_cover);
 
 		} else {
-				//offsethour y la hora 1, seria i=1 (el cero es la acutal)
+			// offsethour y la hora 1, seria i=1 (el cero es la acutal)
 			FIOHourly hourly = new FIOHourly(fio);
 			// In case there is no hourly data available
-			if(hourly.hours() <offsethour)offsethour=hourly.hours()-1;
+			if (hourly.hours() < offsethour)
+				offsethour = hourly.hours() - 1;
 
-			FIODataPoint currently=hourly.getHour(offsethour);
-			dewpoint_c=String.valueOf(currently.dewPoint());
-			temp_c=String.valueOf(currently.temperature());
-			wind_speed_kt=String.valueOf(currently.windSpeed());
-			wind_dir_degrees=String.valueOf(currently.windBearing());
-			String cloud=String.valueOf(currently.cloudCover());
-			Double clou=Double.valueOf(cloud);
-			sky_cover="OVC";
-			if(clou<0.7)sky_cover="BKN";
-			if(clou<0.5)sky_cover="SCT";
-			if(clou<0.25)sky_cover="FEW";
-			if(clou<0.1)sky_cover="SKC";
+			FIODataPoint currently = hourly.getHour(offsethour);
+			dewpoint_c = String.valueOf(currently.dewPoint());
+			temp_c = String.valueOf(currently.temperature());
+			wind_speed_kt = String.valueOf(currently.windSpeed());
+			wind_dir_degrees = String.valueOf(currently.windBearing());
+			String cloud = String.valueOf(currently.cloudCover());
+			Double clou = Double.valueOf(cloud);
+			sky_cover = "OVC";
+			if (clou < 0.7)
+				sky_cover = "BKN";
+			if (clou < 0.5)
+				sky_cover = "SCT";
+			if (clou < 0.25)
+				sky_cover = "FEW";
+			if (clou < 0.1)
+				sky_cover = "SKC";
 
+			return new WeatherDate(temp_c, dewpoint_c, wind_dir_degrees, wind_speed_kt, sky_cover);
 		}
-
-
-
 
 	}
 }
