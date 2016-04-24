a4cfc4309b3bafa161ca051958b517a9470fc703
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-04-24 12:08:11 +0200

    no lfights category

--- src/main/java/sv/dataprocess/DataExtractor.java
+++ src/main/java/sv/dataprocess/DataExtractor.java
@@ -14,8 +14,6 @@ import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -26,66 +24,236 @@ import com.jaunt.UserAgent;
 
 public class DataExtractor {
 
-	private static Logger logger = LoggerFactory.getLogger(DataExtractor.class);
+	public static void main(String[] args) {
 
-		public static void main(String[] args) {
+		PrintWriter pw = null;
+		try {
+			if (new File("Features.txt").exists()) {
+				new File("Features.txt").delete();
+			}
+			pw = new PrintWriter(new FileWriter(new File("Features.txt")));
+		} catch (Exception a) {
+		}
+		writeData(pw, "KJFK", "JFK", "America/New_York", "-0400");
+		writeData(pw, "KLAX", "LAX", "America/Los_Angeles", "-0700");
+		writeData(pw, "KMIA", "MIA", "America/Chicago", "-0500");
+		writeData(pw, "KDAL", "DAL", "America/Chicago", "-0500");
+		writeData(pw, "KAUS", "AUS", "America/Chicago", "-0500");
+		writeData(pw, "KORD", "ORD", "America/Chicago", "-0500");
+		writeData(pw, "KSEA", "SEA", "America/Los_Angeles", "-0700");
+		writeData(pw, "KBOS", "BOS", "America/New_York", "-0400");
+		writeData(pw, "KMSY", "MSY", "America/Chicago", "-0500");
+		writeData(pw, "KPWM", "PWM", "America/Los_Angeles", "-0700");
+		writeData(pw, "KDTW", "DTW", "America/New_York", "-0400");
+		writeData(pw, "KLAS", "LAS", "America/Los_Angeles", "-0700");
+		writeData(pw, "KPDX", "PDX", "America/Phoenix", "-0700");
 
-			try {
-				// longitu, latitud, offsetdia, offsethora
-				WeatherDate weather = getTimeWheater("41.5", "0.5", 0, 0);
+		pw.close();
+	}
 
-			} catch (Exception a) {
+	public static WeatherDate getInfo(URL url) throws Exception {
+		URLConnection con = url.openConnection();
+
+		InputStream in = con.getInputStream();
+
+		String temp_c = "";
+		String dewpoint_c = "";
+		String wind_dir_degrees = "";
+		String wind_speed_kt = "";
+		// String visibility_statute_mi = "";
+		String sky_cover = "";
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(in);
+		doc.getDocumentElement().normalize();
+
+		NodeList nList = doc.getElementsByTagName("METAR");
+		Node nodeData = nList.item(nList.getLength() - 1);
+		NodeList info = nodeData.getChildNodes();
+
+		for (int temp = 0; temp < info.getLength(); temp++) {
+			Node nNode = info.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				try {
+					Element eElement = (Element) nNode;
+					if (eElement.getNodeName().contains("sky_condition")) {
+						Node node = eElement.getAttributes().getNamedItem("sky_cover");
+						sky_cover = node.getNodeValue();
+					} else {
+
+						if (eElement.getNodeName().contains("wind_speed_kt"))
+							wind_speed_kt = eElement.getFirstChild().getNodeValue();
+						if (eElement.getNodeName().contains("wind_dir_degrees"))
+							wind_dir_degrees = eElement.getFirstChild().getNodeValue();
+						if (eElement.getNodeName().contains("temp_c"))
+							temp_c = eElement.getFirstChild().getNodeValue();
+						if (eElement.getNodeName().contains("dewpoint_c"))
+							dewpoint_c = eElement.getFirstChild().getNodeValue();
+					}
+
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+
+				}
+
 			}
 		}
+		Double temp_roc = Double.valueOf(temp_c) - Double.valueOf(dewpoint_c);
+		String cloud = "1";
+		if (sky_cover.contains("CLR")) {
+			cloud = "0";
+		}
+		if (sky_cover.contains("SKC")) {
+			cloud = "0";
+		}
+		if (sky_cover.contains("SCT")) {
+			cloud = "0.4";
+		}
+		if (sky_cover.contains("FEW")) {
+			cloud = "0.2";
+		}
+		if (sky_cover.contains("BKN")) {
+			cloud = "0.7";
+		}
+		return new WeatherDate(wind_speed_kt, String.valueOf(temp_roc), cloud);
+	}
 
-		/*
-		 * pasan la hora del lugar
-		 */
-		public static WeatherDate getTimeWheater(String lon, String lat, int offsetdia, int offsethour) throws Exception {
-			// Gets the hour offset
+	public static void writeData(PrintWriter pw, String station, String stationVisit, String zoneid,
+			String desfaseHorario) {
+		try {
+			Instant now = Instant.now();
+			ZoneId zoneId = ZoneId.of(zoneid);// Los_Angeles //-7
+			ZonedDateTime dateAndTime = ZonedDateTime.ofInstant(now, zoneId);
+			int currentHour = dateAndTime.getHour(); // los angeles
+			UserAgent userAgent = new UserAgent(); // create new userAgent
+													// (headless browser)
+			userAgent
+					.visit("http://tracker.flightview.com/FVAccess2/tools/fids/fidsDefault.asp?accCustId=FVWebFids&fidsId=20001&fidsInit=departures&fidsApt="
+							+ stationVisit + "&fidsFilterAl=&fidsFilterArrap="); // visit
+			// google
+			// userAgent.doc.apply("butterflies"); //apply form input (starting
+			// at first editable field)
+			// userAgent.doc.submit("Google Search"); //click submit button
+			// labelled "Google Search"
 
-			// offSetDays,int hour,int minutes
-			ForecastIO fio = new ForecastIO("3e9f39acff087ff700454b625f1f59d3");
-			fio.setUnits(ForecastIO.UNITS_SI);
+			Elements links = userAgent.doc.findEvery("<td>");// .findEvery("<a>");
+																// //find search
+																// result links
+			boolean siguiente = false;
+			ArrayList<ArrayList<String>> horas = new ArrayList<>();
+			ArrayList<String> siguienteHora = new ArrayList<>();
 
-			fio.setLang(ForecastIO.LANG_ENGLISH);
-			// get forecast
-			fio.getForecast(lon, lat);
+			for (com.jaunt.Element link : links) {
+				String campo = link.getText();// &nbsp;AM
 
-			if (offsetdia >= 1) {
-				offsethour += offsetdia * 24;
+				if (siguiente) {
+					if ((campo.contains(";PM") || campo.contains(";AM"))) {
+						// hora real salida
+						campo = campo.replace("&nbsp;", "");
+
+						String hora = campo.split(":")[0];
+						if (campo.contains("PM") && !campo.contains("12:")) {
+							int nueva = Integer.valueOf(hora) + 12;
+							hora = String.valueOf(nueva);
+							campo = hora + ":" + campo.split(":")[1];
+						}
+
+						campo = campo.replace("AM", "");
+						campo = campo.replace("PM", "");
+
+						siguienteHora.add(campo);
+						Integer horaSalidaReal = Integer.valueOf(hora);
+						if (horaSalidaReal <= currentHour) {
+							// si la hora que ha salido o va a salir es la hora
+							// actual o menor, se peude añadir para
+							// aprendizaje
+							horas.add(siguienteHora);
+						}
+					}
+					siguiente = false;
+					siguienteHora = new ArrayList<>();
+				} else {
+					siguiente = false;
+					if (campo.contains(";PM") || campo.contains(";AM")) {
+						// hora scheduled salida
+
+						String hora = campo.split(":")[0];
+						campo = campo.replace("&nbsp;", "");
+
+						if (campo.contains("PM") && !campo.contains("12:")) {
+							int nueva = Integer.valueOf(hora) + 12;
+							hora = String.valueOf(nueva);
+							campo = hora + ":" + campo.split(":")[1];
+
+						}
+						siguiente = true;
+						campo = campo.replace("AM", "");
+						campo = campo.replace("PM", "");
+						campo = campo.replace("&nbsp;", "");
+						siguienteHora.add(campo);
+					}
+				}
 			}
-			String temp_c = "";//
-			String dewpoint_c = "";//
-			String wind_speed_kt = "";//
 
-			if (offsethour == 0) {
-				// tiempo actual
-				FIOCurrently currently = new FIOCurrently(fio);
-				// Print currently data
-				dewpoint_c = String.valueOf(currently.get().dewPoint());
-				temp_c = String.valueOf(currently.get().temperature());
-				wind_speed_kt = String.valueOf(currently.get().windSpeed());
-				String cloud = String.valueOf(currently.get().cloudCover());
-				//// rociio-temp
-				Double temp_roc=Double.valueOf(temp_c)-Double.valueOf(dewpoint_c);
-				return new WeatherDate(wind_speed_kt,String.valueOf(temp_roc), cloud);
+			String year = String.valueOf(dateAndTime.getYear());
+			String month = String.valueOf(dateAndTime.getMonthValue());
+			if (month.length() == 1)
+				month = "0" + month;
+			// ayer
+			String day = String.valueOf(dateAndTime.getDayOfMonth() - 2);
+			if (day.length() == 1)
+				day = "0" + day;
 
-			} else {
-				// offsethour y la hora 1, seria i=1 (el cero es la acutal)
-				FIOHourly hourly = new FIOHourly(fio);
-				// In case there is no hourly data available
-				if (hourly.hours() < offsethour)
-					offsethour = hourly.hours() - 1;
-				FIODataPoint currently = hourly.getHour(offsethour);
-				dewpoint_c = String.valueOf(currently.dewPoint());
-				temp_c = String.valueOf(currently.temperature());
-				wind_speed_kt = String.valueOf(currently.windSpeed());
-				String cloud = String.valueOf(currently.cloudCover());
-				Double temp_roc=Double.valueOf(temp_c)-Double.valueOf(dewpoint_c);
+			for (int i = 0; i < horas.size(); i++) {
+				ArrayList<String> dosHoras = horas.get(i);
+				String supuesta = dosHoras.get(0);
+				String real = dosHoras.get(1);
+				Integer hora1 = Integer.valueOf(supuesta.split(":")[0]);
+				Integer hora2 = Integer.valueOf(real.split(":")[0]);
+				Integer minutos1 = Integer.valueOf(supuesta.split(":")[1]);
+				Integer minutos2 = Integer.valueOf(real.split(":")[1]);
+				int retraso = (hora2 - hora1) * 60;
+				retraso += (minutos2 - minutos1);
 
-				return new WeatherDate(wind_speed_kt,String.valueOf(temp_roc), cloud);
+				String hour = String.valueOf(hora1);
+				if (hour.length() == 1)
+					hour = "0" + hour;
+				String hour2 = String.valueOf(hora1 + 1);
+				if (hour2.length() == 1)
+					hour2 = "0" + hour2;
+				String minutes = String.valueOf(minutos1);
+				if (minutes.length() == 1)
+					minutes = "0" + minutes;
+				String seconds = String.valueOf((int) dateAndTime.getSecond());
+				if (seconds.length() == 1)
+					seconds = "0" + seconds;
+				String timeNow = year + "-" + month + "-" + day + "T" + hour + ":" + minutes + ":" + seconds;
+				String timeNow2 = year + "-" + month + "-" + day + "T" + hour2 + ":" + minutes + ":" + seconds;
+
+				URL url = new URL(
+						"https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&startTime="
+								+ timeNow + desfaseHorario + "&endTime=" + timeNow2 + desfaseHorario + "&stationString="
+								+ station);
+				WeatherDate data = getInfo(url);
+				if (retraso < 0) {
+					// sale antes, puede pasar, pero se pone a cero, pues no es
+					// un retraso
+					// y no tiene que ver la causa de salir antes
+					retraso = 0;
+				}
+
+				try {
+					pw.println(retraso + " " + data.wind_speed_kt + " " + data.temp_cMinusDewPoint + " "
+							+ data.sky_cover + " ");
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
 
 		}
 	}
+}
