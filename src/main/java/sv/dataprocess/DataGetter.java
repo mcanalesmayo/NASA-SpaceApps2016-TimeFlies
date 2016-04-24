76913962b97be4c1c0189da7258140bed5113d38
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-04-24 10:52:19 +0200

    no lfights category

--- src/main/java/sv/dataprocess/DataGetter.java
+++ src/main/java/sv/dataprocess/DataGetter.java
@@ -8,63 +8,79 @@ import com.github.dvdme.ForecastIOLib.ForecastIO;
 public class DataGetter {
 
 	private static Logger logger = LoggerFactory.getLogger(DataGetter.class);
-	
+
 	public static void main(String[] args) {
+
 		try {
-			getTimeWheater("40", "0", 0, 12, 0);
-		} catch (Exception e) {
-			logger.info(e.getMessage());
+			getTimeWheater("41.5", "0.5", 0, 0);
+
+		} catch (Exception a) {
 		}
 	}
 
-	public static void getTimeWheater(String lon, String lat, int offSetDays, int hour, int minutes) throws Exception {
+	/*
+	 * pasan la hora del lugar
+	 */
+	public static void getTimeWheater(String lon, String lat, int offsetdia, int offsethour) throws Exception {
 		// Gets the hour offset
-		Double desfase = 0.0;
 
-		/*
-		 * URL url = new
-		 * URL("https://maps.googleapis.com/maps/api/timezone/xml?location=" +
-		 * lon + "," + lat +
-		 * "&timestamp=1331161200&key=AIzaSyCYBoVo8kKV-4dwSNtqY78cikkvUSxaXtQ");
-		 * URLConnection con = url.openConnection();
-		 *
-		 * InputStream in = con.getInputStream(); DocumentBuilderFactory
-		 * dbFactory = DocumentBuilderFactory.newInstance(); DocumentBuilder
-		 * dBuilder = dbFactory.newDocumentBuilder(); Document doc =
-		 * dBuilder.parse(in); doc.getDocumentElement().normalize();
-		 *
-		 * NodeList nList = doc.getChildNodes(); Node nodeData = nList.item(0);
-		 * NodeList info = nodeData.getChildNodes(); for (int temp = 0; temp <
-		 * info.getLength(); temp++) {
-		 *
-		 * Node nNode = info.item(temp); if (nNode.getNodeType() ==
-		 * Node.ELEMENT_NODE) { try { Element eElement = (Element) nNode;
-		 *
-		 * if (eElement.getNodeName().contains("raw_offset")) desfase =
-		 * Double.valueOf(eElement.getFirstChild().getNodeValue()); desfase =
-		 * desfase / 3600;// given in seconds desfase++;// summer time
-		 *
-		 * } catch (Exception e) { e.printStackTrace(); }
-		 *
-		 * } }
-		 */
-		hour -= desfase;
-		if (hour >= 24) {
-			offSetDays++;
-			hour -= 24;
-		}
-		if (hour < 0) {
-			offSetDays--;
-			hour += 24;
-		}
 		// offSetDays,int hour,int minutes
-	    ForecastIO fio = new ForecastIO("3e9f39acff087ff700454b625f1f59d3");
-	    fio.setUnits(ForecastIO.UNITS_SI);
-	    fio.setLang(ForecastIO.LANG_ENGLISH);
-	    fio.getForecast(lat, lon);
-	    System.out.println("Latitude: "+fio.getLatitude());
-	    System.out.println("Longitude: "+fio.getLongitude());
-	    System.out.println("Timezone: "+fio.getTimezone());
-	    System.out.println("Offset: "+fio.offset());
+		ForecastIO fio = new ForecastIO("3e9f39acff087ff700454b625f1f59d3");
+		fio.setUnits(ForecastIO.UNITS_SI);
+
+		fio.setLang(ForecastIO.LANG_ENGLISH);
+		// get forecast
+		fio.getForecast(lon, lat);
+
+		if (offsetdia >= 1) {
+			offsethour+=offsetdia*24;
+		}
+		String temp_c = "";//
+		String dewpoint_c = "";//
+		String wind_dir_degrees = "";//
+		String wind_speed_kt = "";//
+		String sky_cover = "";//
+
+		if (offsethour == 0) {
+			// tiempo actual
+			FIOCurrently currently = new FIOCurrently(fio);
+			// Print currently data
+			dewpoint_c=String.valueOf(currently.get().dewPoint());
+			temp_c=String.valueOf(currently.get().temperature());
+			wind_speed_kt=String.valueOf(currently.get().windSpeed());
+			wind_dir_degrees=String.valueOf(currently.get().windBearing());
+			String cloud=String.valueOf(currently.get().cloudCover());
+			Double clou=Double.valueOf(cloud);
+			sky_cover="OVC";
+			if(clou<0.7)sky_cover="BKN";
+			if(clou<0.5)sky_cover="SCT";
+			if(clou<0.25)sky_cover="FEW";
+			if(clou<0.1)sky_cover="SKC";
+
+
+		} else {
+				//offsethour y la hora 1, seria i=1 (el cero es la acutal)
+			FIOHourly hourly = new FIOHourly(fio);
+			// In case there is no hourly data available
+			if(hourly.hours() <offsethour)offsethour=hourly.hours()-1;
+
+			FIODataPoint currently=hourly.getHour(offsethour);
+			dewpoint_c=String.valueOf(currently.dewPoint());
+			temp_c=String.valueOf(currently.temperature());
+			wind_speed_kt=String.valueOf(currently.windSpeed());
+			wind_dir_degrees=String.valueOf(currently.windBearing());
+			String cloud=String.valueOf(currently.cloudCover());
+			Double clou=Double.valueOf(cloud);
+			sky_cover="OVC";
+			if(clou<0.7)sky_cover="BKN";
+			if(clou<0.5)sky_cover="SCT";
+			if(clou<0.25)sky_cover="FEW";
+			if(clou<0.1)sky_cover="SKC";
+
+		}
+
+
+
+
 	}
 }
