911896907e82a6c08a61a098fe31973da3b9e2b5
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-04-25 12:52:14 +0200

    enhancement comments

--- src/main/java/sv/dataprocess/DataGetter.java
+++ src/main/java/sv/dataprocess/DataGetter.java
@@ -10,23 +10,25 @@ import com.github.dvdme.ForecastIOLib.ForecastIO;
 
 public class DataGetter {
 
+	/**
+	* Example of execution
+	*/
 	public static void main(String[] args) {
 
 		try {
 			// longitu, latitud, offsetdia, offsethora
 			//WeatherDate weather = getTimeWheater("41.5", "0.5", 0, 0);
 
-		} catch (Exception a) {
-		}
+			} catch (Exception a) {
+			}
 	}
 
 	/*
-	 * pasan la hora del lugar
+	 * Gets the features of a flight given its longitud, latitude, the offset day and offset hour
 	 */
 	public static WeatherDate getTimeWheater(String lon, String lat, int offsetdia, int offsethour) throws Exception {
-		// Gets the hour offset
 
-		// offSetDays,int hour,int minutes
+		// Forecast api
 		ForecastIO fio = new ForecastIO("3e9f39acff087ff700454b625f1f59d3");
 		fio.setUnits(ForecastIO.UNITS_SI);
 
@@ -37,26 +39,26 @@ public class DataGetter {
 		if (offsetdia >= 1) {
 			offsethour += offsetdia * 24;
 		}
-		String temp_c = "";//
-		String dewpoint_c = "";//
-		String wind_speed_kt = "";//
+		String temp_c = "";
+		String dewpoint_c = "";
+		String wind_speed_kt = "";
 
 		if (offsethour == 0) {
-			// tiempo actual
+			// Current time
 			FIOCurrently currently = new FIOCurrently(fio);
-			// Print currently data
+
 			dewpoint_c = String.valueOf(currently.get().dewPoint());
 			temp_c = String.valueOf(currently.get().temperature());
 			wind_speed_kt = String.valueOf(currently.get().windSpeed());
 			String cloud = String.valueOf(currently.get().cloudCover());
-			//// rociio-temp
 			Double temp_roc=Double.valueOf(temp_c)-Double.valueOf(dewpoint_c);
+
 			return new WeatherDate(wind_speed_kt,String.valueOf(temp_roc), cloud);
 
 		} else {
-			// offsethour y la hora 1, seria i=1 (el cero es la acutal)
+			// Prediction in some hours
 			FIOHourly hourly = new FIOHourly(fio);
-			// In case there is no hourly data available
+			
 			if (hourly.hours() < offsethour)
 				offsethour = hourly.hours() - 1;
 			FIODataPoint currently = hourly.getHour(offsethour);
